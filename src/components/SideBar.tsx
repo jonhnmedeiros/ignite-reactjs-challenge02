129597f8e5959c033ee55a2f63adfdf0fa1f134c
Jonathan Felipe Medeiros <jonathan.info@gmail.com>
2021-05-10 12:40:23 -0300

    Changes on content and sidebar made. App finished

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,47 @@
-export function SideBar() {
-  // Complete aqui
+import { useEffect, useState } from 'react';
+
+import { Button } from '../components/Button';
+import { api } from '../services/api';
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface SideBarIndicator {
+  onClick(id: number): void;
+  selected: number;
+}
+
+export function SideBar({ onClick, selected }: SideBarIndicator) {
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>('genres').then(response => {
+      setGenres(response.data);
+    });
+  }, []);
+
+  // Function in App.tsx
+  // function handleClickButton(id: number) {
+  //   setSelectedGenreId(id);
+  // }
+
+  return (
+      <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+        <div className="buttons-container">
+          {genres.map(genre => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => onClick(genre.id)}
+              selected={selected === genre.id}
+            />
+          ))}
+        </div>
+      </nav>
+  )
 }
